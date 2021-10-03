93b153dfb150c8cbc7e41adc9d454fd5e287bcc5
bavathd <dbavath@gmail.com>
2021-10-03 13:26:44 +0530

    change unknown

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -11,7 +11,7 @@
         $headers = "From: ".$emailFrom;
         $txt = "You have receive an email from".$name.".\n\n".$message;
         mail($mailTo, $subject, $txt, $header);
-        header("Location: index.php?mailsend");
+        
 
     }
 
